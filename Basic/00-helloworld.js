b273e8f6d03d8b61239af41f92c59dcdc2196836
Eduardo Gil <egil@mobiletec.net>
2025-06-01 15:26:22 -0500

    variables

--- Basic/00-helloworld.js
+++ Basic/00-helloworld.js
@@ -1,5 +1,5 @@
 
-// console.log("¡Hola, JavaScript!")
+console.log("¡Hola, JavaScript!")
 
 console.log("5")
 console.log(5)
--- Basic/01-variables.js
+++ Basic/01-variables.js
@@ -1,16 +1,4 @@
-/*
-Clase 16 - Variables
-Vídeo: https://youtu.be/1glVfFxj8a4?t=3049
-*/
-
-// var
-
-var helloWorld = "¡Hola, JavaScript!"
-console.log(helloWorld)
-
-helloWorld = "¡Hola de nuevo, JavaScript!"
-console.log(helloWorld)
-
+// Variables in JavaScript
 // let
 
 let helloWorld2 = "¡Hola, JavaScript 2!"
@@ -23,7 +11,3 @@ console.log(helloWorld2)
 
 const helloWorld3 = "¡Hola, JavaScript 3!"
 console.log(helloWorld3)
-
-// Error
-// helloWorld3 = "¡Hola de nuevo, JavaScript 2!"
-// console.log(helloWorld3)
